539e7212f6cc1e9a821940aff05042f1969a043d
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2020-08-25 12:15:19 +0530

    add some demo test cases for the cards

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
+import Cards from './Cards/Cards';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+const setup = () => {
+  const utils = render(<Cards />)
+  const input = utils.getByRole('textbox')
+  return {
+    input,
+    ...utils,
+  }
+}
+
+test('It should keep a 5 in front of the input', () => {
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: '5' } })
+  expect(input.value).toBe('5')
+})
+
+test('It should not allow inputted -(minues)', () => {
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: '-1' } })
+  expect(input.value).toBe('-1') //empty after
+})
+
+test('It should not allow empty', () => {
+  const { input } = setup()
+  fireEvent.change(input, { target: { value: '' } })
+  expect(input.value).toBe('') //empty after
+})
+
+test('Number of people span or div', () => {
+  const { getByText } = render(<Cards />);
+  const textElement = getByText(/Number of people/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Having textbox', () => {
+  render(<Cards />);
+  expect(screen.getByRole('textbox')).toBeInTheDocument();
+});
+
+test('Having button', () => {
+  render(<Cards />);
+  expect(screen.getByRole('button')).toBeInTheDocument();
 });
